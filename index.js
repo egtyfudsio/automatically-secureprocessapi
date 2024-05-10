3a23e0b67ac95c4020dcadf87de6388e5416f5b1
egtyfudsio <gs1farce777@hotmail.com>
2024-05-11 04:41:50 +0700

    hello world

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-function subarraySum(nums, k) {
-  const map = new Map();
-  map.set(0, 1);
-  let count = 0;
-  let sum = 0;
-  for (const num of nums) {
-    sum += num;
-    if (map.has(sum - k)) {
-      count += map.get(sum - k);
-    }
-    map.set(sum, (map.get(sum) || 0) + 1);
-  }
-  return count;
+function add(x, y) {
+  return x + y;
 }
+
+const result = add(5, 3);
+console.log(result); // 8
