0690103fe9f1986d4dbcca9b00dee0395d90f561
egtyfudsio <gs1farce777@hotmail.com>
2024-05-13 09:12:55 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function singleNumber(nums) {
-  let result = 0;
-  for (const num of nums) {
-    result ^= num;
-  }
-  return result;
-}
+const senderSignature = aptos.transaction.sign({ signer: alice, transaction });
+
+// Sponsor signs
+const sponsorSignature = aptos.transaction.signAsFeePayer({
+  signer: sponsor,
+  transaction,
+});
+
+// Submit the transaction to chain
+const committedTxn = await aptos.transaction.submit.simple({
+  transaction,
+  senderAuthenticator: senderSignature,
+  feePayerAuthenticator: sponsorSignature,
+});
